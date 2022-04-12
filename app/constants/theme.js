1576cf83654b32e2e36c78a57c6d14de2d23b650
Minh Trang <minhtrang.9096@gmail.com>
2022-04-12 21:02:07 +0700

    Login UI

--- app/constants/theme.js
+++ app/constants/theme.js
@@ -46,8 +46,11 @@ export const SIZES = {
 export const FONTS = {
     largeTitle: { fontFamily: "Roboto-Black", fontSize: SIZES.largeTitle, lineHeight: 55 },
     h1: { fontFamily: "Roboto-Black", fontSize: SIZES.h1, lineHeight: 36 },
+    h1_light: { fontFamily: "Roboto-Light", fontSize: SIZES.h1, lineHeight: 36 },
     h2: { fontFamily: "Roboto-Bold", fontSize: SIZES.h2, lineHeight: 30 },
+    h2_light: { fontFamily: "Roboto-Light", fontSize: SIZES.h2, lineHeight: 30 },
     h3: { fontFamily: "Roboto-Bold", fontSize: SIZES.h3, lineHeight: 22 },
+    h3_light: { fontFamily: "Roboto-Light", fontSize: SIZES.h3, lineHeight: 22 },
     h4: { fontFamily: "Roboto-Bold", fontSize: SIZES.h4, lineHeight: 22 },
     body1: { fontFamily: "Roboto-Regular", fontSize: SIZES.body1, lineHeight: 36 },
     body2: { fontFamily: "Roboto-Regular", fontSize: SIZES.body2, lineHeight: 30 },
--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { View, Text, StyleSheet, SafeAreaView, ImageBackground, Platform, StatusBar } from "react-native";
-import { FONTS, images } from "../constants";
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    ImageBackground,
+    Platform,
+    StatusBar,
+    TouchableOpacity,
+    KeyboardAvoidingView
+} from "react-native";
+import { FONTS, images, SIZES, COLORS } from "../constants";
+import { FontAwesome, Feather } from '@expo/vector-icons';
 
-const Login = () => {
+const Login = ({ navigation }) => {
+
+    const [hide, setHide] = React.useState(true);
 
     React.useEffect(() => {
         if (Platform.OS === "android") {
@@ -16,11 +29,101 @@ const Login = () => {
         return (
             <View
                 style={{
-                    flex: 1,
+                    marginTop: SIZES.height * 0.2,
+                    padding: SIZES.padding,
+                    justifyContent: 'center'
                 }}
             >
 
-            </View>
+                <Text style={{ ...FONTS.h2_light, color: COLORS.black }}>Email</Text>
+                <View
+                    style={{
+                        flexDirection: "row",
+                        alignItems: 'center',
+                        borderWidth: 1,
+                        borderColor: COLORS.black,
+                        borderRadius: SIZES.radius,
+                        padding: SIZES.base * 2,
+                        marginVertical: SIZES.base
+                    }}
+                >
+                    <View
+                        style={{
+                            marginRight: SIZES.base
+                        }}
+                    >
+                        <FontAwesome name="user" size={30} color={COLORS.black} />
+                    </View>
+
+                    <TextInput
+                        placeholder="Your Email ..."
+                        placeholderTextColor={COLORS.black}
+                        style={{
+                            ...FONTS.h3_light,
+                            color: COLORS.black
+                        }}
+                    />
+                </View>
+                {/* Password */}
+                <Text style={{ ...FONTS.h2_light, color: COLORS.black }}>Password</Text>
+                <View
+                    style={{
+                        flexDirection: "row",
+                        alignItems: 'center',
+                        borderWidth: 1,
+                        borderColor: COLORS.black,
+                        borderRadius: SIZES.radius,
+                        padding: SIZES.base * 2,
+                        marginVertical: SIZES.base
+                    }}
+                >
+                    <View
+                        style={{
+                            marginRight: SIZES.base
+                        }}
+                    >
+                        <FontAwesome name="lock" size={30} color={COLORS.black} />
+                    </View>
+
+                    <TextInput
+                        placeholder="Your Password ..."
+                        placeholderTextColor={COLORS.black}
+                        secureTextEntry={hide}
+                        style={{
+                            ...FONTS.h3_light,
+                            color: COLORS.black,
+                            flex: 1
+                        }}
+                    />
+
+                    <TouchableOpacity
+                        onPress={() => setHide(!hide)}
+                    >
+                        {
+                            hide ?
+                                <Feather name="eye-off" size={30} color="black" />
+                                :
+                                <Feather name="eye" size={30} color="black" />
+                        }
+
+                    </TouchableOpacity>
+                </View>
+
+                <TouchableOpacity
+                    style={{
+                        marginTop: SIZES.padding,
+                        backgroundColor: COLORS.black,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        paddingVertical: SIZES.base * 2,
+                        borderRadius: SIZES.radius
+
+                    }}
+                    onPress={() => navigation.navigate('SignIn')}
+                >
+                    <Text style={{ ...FONTS.h2, color: COLORS.white }}>Login</Text>
+                </TouchableOpacity>
+            </View >
         )
     }
 
