02aca0816413e35c0d2db472535df19cad5f6b11
Minh Trang <minhtrang.9096@gmail.com>
2022-04-22 11:49:18 +0700

    add expoToken push notifications

--- app/utils/services.js
+++ app/utils/services.js
@@ -1,7 +1,10 @@
 import { auth, database } from "../firebase/firebase";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { ref, set, push, onValue, remove, } from "firebase/database";
+import { ref, set, push, onValue, remove, update } from "firebase/database";
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+
 
 
 
@@ -9,9 +12,11 @@ const signUpUser = (email, password, callback) => {
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             const user = userCredential.user;
+            // createUser(user);
             callback({ result: "success", user: user.email });
         })
         .catch((error) => {
+            console.log("error");
             callback({ result: "fail", error: error });
             console.log(error);
         })
@@ -21,6 +26,7 @@ const signInUser = (email, password, callback) => {
     signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             const user = userCredential.user;
+            //registerForPushNotificationsAsync(user);
             callback({ result: "success", data: { email: user.email, id: user.uid } });
         })
         .catch((error) => {
@@ -47,6 +53,20 @@ const getUserData = () => {
 const setUserData = async (data) => {
     data = JSON.stringify(data);
     return AsyncStorage.setItem('user', data);
+}
+
+const createUser = (user) => {
+    console.log('come here');
+    set(ref(database, 'users/' + user.uid), {
+        email: user.email,
+        createdAt: user.createdAt
+    })
+        .then(() => {
+            console.log('Success');
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 const addnewTask = (userId, data) => {
@@ -93,6 +113,49 @@ const updateTodo = (userId, data) => {
         });
 }
 
+const registerForPushNotificationsAsync = async (user) => {
+    let token;
+    if (Device.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log(token);
+    } else {
+        alert('Must use physical device for Push Notifications');
+    }
+
+    if (Platform.OS === 'android') {
+        Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+        });
+    }
+
+    const updateData = {
+        expoToken: token
+    };
+
+    update(ref(database, 'users/' + user.uid), {
+        ...updateData
+    })
+        .then(() => {
+            console.log("Save token success");
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+}
+
 export {
     signUpUser,
     signInUser,
@@ -102,5 +165,6 @@ export {
     getUserData,
     addnewTask,
     deleteTodo,
-    updateTodo
+    updateTodo,
+    createUser
 }
